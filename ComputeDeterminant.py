cea6735a24479fc921c2d96d5ddca526ef7399dc
sylvest99js <sylvest@uw.edu>
2021-04-15 08:51:48 -0700

    Fixed basic error repalcing A[1][mmm] with A[0][mmm] and commented out some print statements

--- ComputeDeterminant.py
+++ ComputeDeterminant.py
@@ -1,27 +1,28 @@
-def determinant(A):
-    if len(A) == 1:
-        return A[0][0]
+def determinant(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
 
-    tempA = A[1 : len(A)]
+    tempmatrix = matrix[1: len(matrix)]
 
     det = 0
     factor = -1
-    for mmm in range(len(A)):
+    for mmm in range(len(matrix)):
         minor = []
         factor *= -1  # alternate sighs
-        for nn in range(len(tempA)):
-            aa = tempA[nn].copy()
+        for nn in range(len(tempmatrix)):
+            aa = tempmatrix[nn].copy()
             aa.pop(mmm)
             minor.append(aa)
-
-        det += factor * A[1][mmm] * determinant(minor)
-        print("detsofar", det)
+        dm = determinant(minor)
+        det += factor * matrix[0][mmm] * dm
+        # print("minor", minor)
+        # print("determinant(minor)", dm)
 
     return det
 
 
-B = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+B = [[1, 3, 2], [0, 3, 6], [0, 0, 1]]
 
 C = B
 
-print(determinant(B))
+print("determinant(B) = ", determinant(B))
