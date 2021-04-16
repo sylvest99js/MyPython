cd43876c3c1c3b93e44b2062dc149833caa91dff
sylvest99js <sylvest@uw.edu>
2021-04-16 14:04:07 -0700

    Added Comments to explain construction of minors

--- ComputeDeterminant.py
+++ ComputeDeterminant.py
@@ -2,27 +2,31 @@ def determinant(matrix):
     if len(matrix) == 1:
         return matrix[0][0]
 
-    tempmatrix = matrix[1: len(matrix)]
+    without_row_1 = matrix[1: len(matrix)]
 
     det = 0
-    factor = -1
+    factor = -1 # first term in sum should have  + sign
     for mmm in range(len(matrix)):
         minor = []
-        factor *= -1  # alternate sighs
-        for nn in range(len(tempmatrix)):
-            aa = tempmatrix[nn].copy()
+        # Now build the minor by deleting the mmm column
+        # We build row by row because we don't know a
+        # way to make a deep copy of a list of lists
+        for nn in range(len(without_row_1)):
+            aa = without_row_1[nn].copy()
             aa.pop(mmm)
             minor.append(aa)
+            
         dm = determinant(minor)
+        factor *= -1  # alternate signs in sum
         det += factor * matrix[0][mmm] * dm
+        # Just for testing
         # print("minor", minor)
         # print("determinant(minor)", dm)
 
     return det
 
 
-B = [[1, 3, 2], [0, 3, 6], [0, 0, 1]]
+B = [ [2, 3, 2], [4, 3, 6], [0, 0, 4] ]
 
-C = B
 
 print("determinant(B) = ", determinant(B))
